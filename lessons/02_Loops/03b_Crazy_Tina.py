1cd291f9ee091cff6612c28860aa35fa31e65b5b
ConnorGuerena <connor.guerena@students.jointheleague.org>
2024-11-12 23:46:52 +0000

    hsggjfg;hrgpierwg

--- lessons/02_Loops/03b_Crazy_Tina.py
+++ lessons/02_Loops/03b_Crazy_Tina.py
@@ -14,32 +14,22 @@ more help
 
 
 import turtle                           # Tell Python we want to work with the turtle
-turtle.setup (width=600, height=600)    # Set the size of the window
+turtle.setup (width=700, height=600)    # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
 
 tina.shape('turtle')                    # Set the shape of the turtle to a turtle
-tina.speed(0)                           # Make the turtle move as fast, but not too fast. 
+tina.speed(1)                           # Make the turtle move as fast, but not too fast. 
 
 
 forwards = [ 50,30,50,30 ]*50
 lefts = [ 45,90,45,90 ]*50
-colors = ['blue', 'red', 'orange', 'black', ]*50
 
-for  x in range(20):
-    for  i in range(20):
+tina.write('Tina was not the Imposter')
+for c in range(50):
+    tina.forward(c/10)
+    tina.tilt(10)
 
-        forward = forwards[i]
-        left = lefts[i]
-        color = colors[i]
-
-
-        tina.color(color)
-        tina.forward(forward)
-        tina.left(left)
-        tina.forward(i)
-    tina.left(forward-x)
-    tina.forward(left-i)
 
 
 
